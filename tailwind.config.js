2eadfec655812fbaed10a6e42374286a9585956a
maha-ahmad1 <maha.ahmad.k@hotmail.com>
2023-04-17 16:16:15 +0300

    add section Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./src/*/.{html,js}", "./index.html"],
   theme: {
     extend: {
       colors: {
         'custom-gray': '#060606',
         'green':'#A2AA7B',
         'gray':'#556260',
+        'blue':'#0F172A',
+        'gray-100':'#E4E4E7'
       },
       textTransform: {
         uppercase: 'uppercase',
